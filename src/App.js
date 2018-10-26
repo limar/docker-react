46eb4811abc346793242582d44e8213913d99612
Mark Lifshits <limar.go@gmail.com>
2018-10-26 21:01:00 +0800

    changed text as of Featured

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Sometimes edit <code>src/App.js</code> and save to reload.
+            [Featured] Sometimes edit <code>src/App.js</code> and save to reload.
           </p>
           <a
             className="App-link"
